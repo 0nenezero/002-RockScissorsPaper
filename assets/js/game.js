d156e945b4fde35246e265d0a165f64333751f40
0nenezero <tempo9134@naver.com>
2024-05-05 18:41:34 +0900

    :sparkles: Feat: 가위바위보 게임 설정

--- assets/js/game.js
+++ assets/js/game.js
@@ -8,15 +8,62 @@ const game = {
   totalRound: 10,
 };
 
-const $choiceBtns = document.querySelectorAll(".choice-btn");
+const $gameCont = document.getElementById("game-cont");
+
+const $choiceBtns = $gameCont.querySelectorAll(".choice-btn");
 $choiceBtns.forEach((choice) => {
   choice.addEventListener("click", (e) => {
-    console.log(e.target.innerText);
+    playGame(e.target.innerText);
   });
 });
 
-const $round = document.querySelector(".round-cont span");
-function renderRound() {
-  $round.textContent = game.totalRound - game.round + 1;
+function playGame(userChoice) {
+  if (game.round > game.totalRound) return;
+
+  const computerChoice = choice[Math.floor(Math.random() * choice.length)];
+  renderChoice(userChoice, computerChoice);
+  if (computerChoice === userChoice) {
+    console.log("무승부");
+  } else if (
+    (computerChoice === "가위" && userChoice === "바위") ||
+    (computerChoice === "바위" && userChoice === "보") ||
+    (computerChoice === "보" && userChoice === "가위")
+  ) {
+    console.log("you win");
+    score.user++;
+  } else {
+    console.log("computer win");
+    score.computer++;
+  }
+  renderScore();
+  renderCount();
+  game.round++;
+  if (game.round > game.totalRound) {
+    renderResult();
+  }
 }
-renderRound();
+
+const $userChoice = $gameCont.querySelector(".user .choice");
+const $computerChoice = $gameCont.querySelector(".computer .choice");
+function renderChoice(user, computer) {
+  $userChoice.innerText = user;
+  $computerChoice.innerText = computer;
+}
+
+const $userPlayScore = $gameCont.querySelector(".user .score");
+const $computerPlayScore = $gameCont.querySelector(".computer .score");
+function renderScore() {
+  $userPlayScore.innerText = score.user;
+  $computerPlayScore.innerText = score.computer;
+}
+
+const $gameCount = $gameCont.querySelector(".round-cont span");
+function renderCount() {
+  $gameCount.innerText = game.totalRound - game.round;
+}
+
+function initGame() {
+  renderScore();
+  renderCount();
+}
+initGame();
